c3456917bae6e4aba548c28cfb3752fda81b3910
prapoju <prapoju@gmail.com>
2026-05-09 15:19:32 -0500

    test: add integration test path in build.gradle

--- services/circleguard-promotion-service/build.gradle.kts
+++ services/circleguard-promotion-service/build.gradle.kts
@@ -6,6 +6,31 @@ plugins {
     kotlin("plugin.jpa")
 }
 
+java {
+    sourceSets {
+        create("integrationTest") {
+            compileClasspath += sourceSets["main"].output + sourceSets["test"].output
+            runtimeClasspath += sourceSets["main"].output + sourceSets["test"].output
+        }
+    }
+}
+
+configurations {
+    val integrationTestImplementation by getting {
+        extendsFrom(configurations["testImplementation"])
+    }
+    val integrationTestRuntimeOnly by getting {
+        extendsFrom(configurations["runtimeOnly"])
+    }
+}
+
+tasks.named<Test>("integrationTest") {
+    testClassesDirs = sourceSets["integrationTest"].output.classesDirs
+    classpath = sourceSets["integrationTest"].runtimeClasspath + sourceSets["main"].runtimeClasspath
+    shouldRunAfter("test")
+    outputs.upToDateWhen { false }
+}
+
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
     testImplementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
